217970691551124f25a04d53eb8b4cb85cbdd636
977ADAM <977team.95@gmail.com>
2026-04-06 15:15:12 +0300

    .

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from src.core.errors.exception_handler import AppException
 from src.core.logging.logger import setup_logging, get_logger
+from src.pipeline.train_pipeline import TrainingPipeline
 
 setup_logging(app_name="recsys", level="DEBUG")
 logger = get_logger(__name__)
 
 def main():
     try:
-        a = 1/0
+        training_pipeline = TrainingPipeline()
+        training_pipeline.start_training_pipeline()
     except Exception as e:
         logger.error(f"An error occurred: {e}")
         raise AppException(e)
